72e7764521ad66029e735cf550936f7831f58064
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-03-03 00:42:46 +0530

    logic

--- ARRAY/min_max_arr.py
+++ ARRAY/min_max_arr.py
@@ -1,3 +1,8 @@
+# 1️⃣ Find Maximum & Minimum
+# Given an array of numbers, return max and min.
+
+
+
 arr = [6,3,2,8,1,4]
 
 
@@ -15,3 +20,16 @@ for i in range(1,len(arr)):
     if arr[i]<min:
         min = arr[i]
 print("min is ",min)
+
+
+
+# both 
+max_value = arr[0]
+min_value = arr[0]
+for i in range(1,len(arr)):
+    if arr[i]>max_value:
+        max_value = arr[i]
+    if arr[i]< min_value:
+        min_value = arr[i]
+print("max is ",max_value)
+print("min is ",min_value)
