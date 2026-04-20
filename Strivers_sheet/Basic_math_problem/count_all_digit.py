6b712dea3e45ab4a8c87784e807b9c37bdda4dca
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-04-21 00:06:28 +0530

    basic math

---
+++ Strivers_sheet/Basic_math_problem/count_all_digit.py
@@ -0,0 +1,20 @@
+# Problem Statement: Given an integer N, return the number of digits in N.
+
+# Example 1:
+# Input:N = 12345
+# Output:5
+# Explanation:  The number 12345 has 5 digits.
+                        
+def count(n):
+    n = abs(n)
+    if n==0:
+        print(1)
+        return 1
+   
+    count =0
+    while n>0:
+        n=n//10
+        count = count+1
+    print(count)
+
+count(-0)
