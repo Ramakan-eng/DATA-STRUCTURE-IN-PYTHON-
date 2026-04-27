f21c5f8ee07fa3d2841d9dbafcca01bc1a3fe913
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-04-27 23:48:31 +0530

    gcd

--- Strivers_sheet/Basic_math_problem/gcd.py
+++ Strivers_sheet/Basic_math_problem/gcd.py
@@ -7,5 +7,22 @@ class gcd:
         print(a)
         return a
     
+    def by_broute(self,a,b):
+        gcd =1
+        if a<b:
+            for i in range(1,a):
+                if a%i ==0 and b % i ==0:
+                    gcd = i
+            print(gcd)
+            return gcd
+        else:
+            for i in range(1,b):
+                if a%i ==0 and b%i==0:
+                    gcd = i
+            print(gcd)
+            return gcd
+
+    
 obj = gcd()
-obj.gcd_cal(9,12)
+# obj.gcd_cal(9,12)
+obj.by_broute(9,12)
