656b2bff685037d4f647026aba72742a375707f2
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-04-28 23:28:38 +0530

    all divisor

---
+++ Strivers_sheet/Basic_math_problem/all_diviser.py
@@ -0,0 +1,27 @@
+# Print all Divisors of a given Number
+
+
+# 17
+
+# Problem Statement: Given an integer N, return all divisors of N.
+# A divisor of an integer N is a positive integer that divides N without leaving a remainder. In other words, if N is divisible by another integer without any remainder, then that integer is considered a divisor of N.
+
+# Examples
+# Input: N = 36
+# Output: [1, 2, 3, 4, 6, 9, 12, 18, 36]  
+# Explanation: The divisors of 36 are 1, 2, 3, 4, 6, 9, 12, 18, 36.
+# Input: N = 12
+# Output: [1, 2, 3, 4, 6, 12]
+# Explanation: The divisors of 12 are 1, 2, 3, 4, 6, 12.
+
+def all_diveser(num):
+    diviser =1 
+    list_div =[]
+    while diviser <= num:
+        remi = num % diviser
+        if remi ==0:
+            list_div.append(diviser)
+        diviser = diviser + 1
+    print(list_div)
+
+all_diveser(160)
