0a485e0388f1c2d998532e71924e760e55304551
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-04-30 23:00:03 +0530

    first natural number

---
+++ Strivers_sheet/Basic_recursion/sum_first_natural.py
@@ -0,0 +1,31 @@
+# Sum of first N Natural Numbers
+
+
+# 9
+
+# Problem Statement: Given a number ‘N’, find out the sum of the first N natural numbers .
+
+# Examples
+# Input: N=5
+# Output: 15
+# Explanation: 1+2+3+4+5=15
+
+# Input: N=6
+# Output: 21
+# Explanation: 1+2+3+4+5+6=15
+
+# by for loop 
+
+class First_NSum: 
+    def by_loop(self,n):
+        total = 0 
+        for i in range(1,n+1):
+            total = total + i
+        print(total)
+        return total
+    
+
+obj = First_NSum()
+obj.by_loop(5)
+
+
