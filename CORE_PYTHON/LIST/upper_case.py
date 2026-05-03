d73fb70f2f5b20eb3c90893860ece5ed0b244cfb
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-05-03 09:32:30 +0530

    upper case convert

---
+++ CORE_PYTHON/LIST/upper_case.py
@@ -0,0 +1,6 @@
+# Convert a list of strings ["apple", "banana", "cherry"] to uppercase using list comprehension.
+
+in_string = ["apple", "banana", "cherry"]
+
+upper_case = [i.upper() for i in in_string]
+print(upper_case)
