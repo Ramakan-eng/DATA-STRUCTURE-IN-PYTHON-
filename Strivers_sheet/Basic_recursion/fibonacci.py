5ef6189b5d74cedf5430433ee424794f92512697
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-05-12 23:51:40 +0530

    fibonacci

--- Strivers_sheet/Basic_recursion/fibonacci.py
+++ Strivers_sheet/Basic_recursion/fibonacci.py
@@ -9,6 +9,19 @@ def fibonacci(num):
             b = next
             n = n + 1
             
-        
+# fibonacci(5)
 
-fibonacci(5)
+def fibonaccis(N):
+
+    if N <= 1:
+        print(f"base case {N}")
+        return N
+    
+    last = fibonaccis(N - 1)   # (N-1)th term
+    slast = fibonaccis(N - 2)  # (N-2)th term
+    print(f"last {last} and slast {slast} :",last + slast)
+    return last + slast
+
+# Driver code
+N = 4
+print(fibonaccis(N))  
