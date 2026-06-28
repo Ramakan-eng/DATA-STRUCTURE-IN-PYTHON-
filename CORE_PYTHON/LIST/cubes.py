75fe461fdd8ff54fd56043e1aaf3e63bdd7119a5
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-06-28 09:39:12 +0530

    practice questions

---
+++ CORE_PYTHON/LIST/cubes.py
@@ -0,0 +1,11 @@
+# Given:
+
+nums = [1,2,3,4,5]
+
+# Create a list of cubes.
+
+# Expected Output
+
+# [1,8,27,64,125]
+cubes = [i**3 for i in nums]
+print(cubes)
--- CORE_PYTHON/LIST/upper_case.py
+++ CORE_PYTHON/LIST/upper_case.py
@@ -4,3 +4,14 @@ in_string = ["apple", "banana", "cherry"]
 
 upper_case = [i.upper() for i in in_string]
 print(upper_case)
+
+###############33
+
+# Given:
+
+names = ["ram","sita","gopal"]
+
+# Convert all names to uppercase using list comprehension.
+
+upper = [i.upper() for i in names]
+print(upper)
