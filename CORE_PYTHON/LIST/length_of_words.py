4d2394bb91bcd0a25d1a274f21beca13d1a6645c
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-06-28 09:20:54 +0530

    length of words

---
+++ CORE_PYTHON/LIST/length_of_words.py
@@ -0,0 +1,12 @@
+# Given:
+
+words = ["apple","banana","cat","dog"]
+
+# Create a list containing the length of each word.
+
+# Expected Output
+
+# [5,6,3,3]
+
+length = [len(i) for i in words]
+print(length)
