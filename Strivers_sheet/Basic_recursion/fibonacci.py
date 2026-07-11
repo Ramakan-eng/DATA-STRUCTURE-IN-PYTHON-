9f932852303370ca9632e3dec7ac0159991770fb
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-07-11 22:27:17 +0530

    better approach for fibonacchi

--- Strivers_sheet/Basic_recursion/fibonacci.py
+++ Strivers_sheet/Basic_recursion/fibonacci.py
@@ -1,3 +1,5 @@
+# Broute force approach
+
 def fibonnaci(num):
     if num == 0:
         print(0)
@@ -12,4 +14,26 @@ def fibonnaci(num):
 
         print(f"fibonnacci series of {num} is : ")
         print(" ".join(str(n) for n in fib))
-fibonnaci(6)
+# fibonnaci(6)
+
+# Better approach 
+
+def fib(n):
+    if n<=1:
+        print(n,end=" ")
+        return n 
+    else:
+        last_num= 0
+
+        second_num = 1
+        print(f"fibonacci series of the {n} is : ")
+        print(last_num,second_num, end = " ")
+        for i in range(2,n+1):
+            curtent_val = last_num + second_num
+            last_num, second_num = second_num , curtent_val
+
+            
+    
+            print(curtent_val, end = " ")        
+        
+fib(4)
