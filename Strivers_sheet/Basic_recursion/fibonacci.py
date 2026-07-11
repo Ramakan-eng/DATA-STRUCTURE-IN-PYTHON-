40d45fec4757cb4b81b02b2faeb9e31860ec82ad
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-07-11 18:59:46 +0530

    fibonacci series

--- Strivers_sheet/Basic_recursion/fibonacci.py
+++ Strivers_sheet/Basic_recursion/fibonacci.py
@@ -1,42 +1,15 @@
-def fibonacci(num):
-    a =0
-    b=1
-    n =0
-    while n <= num:
-            print(a)
-            next = a+b
-            a= b 
-            b = next
-            n = n + 1
-            
-# fibonacci(5)
+def fibonnaci(num):
+    if num == 0:
+        print(0)
+    elif num ==1:
+        print("0,1")
+    else: 
+        fib =[0] * (num+1)
+        fib[0]=0
+        fib[1]=1
+        for i in range(2,num+1):
+            fib[i] = fib[i-1]+fib[i-2]
 
-
-def fibonacci(n):
-    a, b = 0, 1
-
-    for i in range(n):
-        a, b = b, a + b
-        print(a)
-    
-    return a
-
-print(fibonacci(4))
-
-
-
-
-def fibonaccis(N):
-
-    if N <= 1:
-        print(f"base case {N}")
-        return N
-    
-    last = fibonaccis(N - 1)   # (N-1)th term
-    slast = fibonaccis(N - 2)  # (N-2)th term
-    print(f"last {last} and slast {slast} :",last + slast)
-    return last + slast
-
-# Driver code
-N = 4
-# print(fibonaccis(N))  
+        print(f"fibonnacci series of {num} is : ")
+        print(" ".join(str(n) for n in fib))
+fibonnaci(6)
