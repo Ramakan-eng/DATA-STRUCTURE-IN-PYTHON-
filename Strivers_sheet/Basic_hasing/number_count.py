56ca947e1c0e08a99310402b536da02a41d2056e
RAMAKANT SAHU <ramakantsahu895@gmail.com>
2026-07-19 00:43:35 +0530

    hash

--- Strivers_sheet/Basic_hasing/number_count.py
+++ Strivers_sheet/Basic_hasing/number_count.py
@@ -7,22 +7,22 @@
 # 	      5 occurs 2 times in the array
 #               15 occurs 1 time in the array
 
-## BROUTE FORCE TECHNIQUE
+# BROUTE FORCE TECHNIQUE
 
-# def freq_arr(arr, n):
-#    visited_arr = [0] * n
+def freq_arr(arr, n):
+   visited_arr = [0] * n
 
-#    for i in range(n):
-#        if visited_arr[i]:
-#            continue
-#        count =1
-#        for j in range(i+1,n):
-#            if arr[i] == arr[j]:
-#                visited_arr[j] = 1
-#                count =count + 1
-#        print(arr[i],":",count,end=" ")
+   for i in range(n):
+       if visited_arr[i]:
+           continue
+       count =1
+       for j in range(i+1,n):
+           if arr[i] == arr[j]:
+               visited_arr[j] = 1
+               count =count + 1
+       print(arr[i],":",count,end=" ")
 
-# freq_arr([10,5,10,15,10,0],6)
+freq_arr([10,5,10,15,10,0],6)
 
 
 # BY DICTIONARY 
@@ -38,5 +38,21 @@ for i in arr:
         freq[i] = 1
 print("frequency of words:",freq)
 
+
+
+# By defaultdictionary
+
+from collections import defaultdict
+def defaultdict_hash(arr,n):
+    freq_map = defaultdict(int)
+
+    for i in range(n):
+        freq_map[arr[i]] += 1
+    
+    for key, value in freq_map.items():
+        print("frequency by defaultdict : ",key,":",value)
+
+
+defaultdict_hash([10,5,10,15,10,5,0],7)
 
 
